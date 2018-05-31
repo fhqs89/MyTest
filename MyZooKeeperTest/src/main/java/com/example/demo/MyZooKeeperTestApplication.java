e7e49be946b25954e7be5a2a36949da5bb8b9cc4
zhanghao <chinazh198907@sina.com>
2018-05-31 23:20:54 +0800

    first

---
+++ MyZooKeeperTest/src/main/java/com/example/demo/MyZooKeeperTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyZooKeeperTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyZooKeeperTestApplication.class, args);
+	}
+}
