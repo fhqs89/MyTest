d4d6f5810e96700b88ca91d3568bae8b2e592491
zhanghao <chinazh198907@sina.com>
2019-12-24 01:06:50 +0800

    netty

---
+++ MyZooKeeperTest/src/main/java/com/example/demo/netty/ch2/EchoServer.java
@@ -0,0 +1,54 @@
+package com.example.demo.netty.ch2;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class EchoServer {
+	
+	private final int port;
+	
+	public EchoServer(int port) {
+		this.port = port;
+	}
+	
+	public void start() throws InterruptedException{
+		final EchoServerHandler serverHandler = new EchoServerHandler();
+		/*线程组*/
+		EventLoopGroup group = new NioEventLoopGroup();
+		try {
+			ServerBootstrap b = new ServerBootstrap();/*服务端启动必备*/
+			b.group(group)
+			.channel(NioServerSocketChannel.class) /*指明使用nio进行通讯*/
+			.localAddress(port)  /*指明服务器监听端口*/
+			/*接收到请求，新启一个socket通信，也就是channel，每个channel都有自己的事件的handler*/
+			.childHandler(new ChannelInitializer<SocketChannel>() {
+
+				@Override
+				protected void initChannel(SocketChannel ch) throws Exception {
+					ch.pipeline().addLast(serverHandler);
+					
+				}
+				
+			});
+			
+			ChannelFuture f = b.bind().sync();/*绑定到端口，阻塞等到直到连接完成*/
+			f.channel().close().sync();
+		}finally {
+			group.shutdownGracefully().sync();
+		}
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		int port = 9999;
+		EchoServer echoServer = new EchoServer(port);
+		System.out.println("服务即将启动");
+		echoServer.start();
+		System.out.println("服务器关闭");
+	}
+
+}
---
+++ MyZooKeeperTest/src/main/java/com/example/demo/netty/ch2/EchoServerHandler.java
@@ -0,0 +1,50 @@
+package com.example.demo.netty.ch2;
+
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * 自己的业务类
+ * @author zhanghao
+ *
+ */
+/*指明我这个Handler可以在多个channel之间共享，意味着这个实现必须是线程安全的*/
+@ChannelHandler.Sharable  
+//无状态
+public class EchoServerHandler extends ChannelInboundHandlerAdapter{
+
+	/**
+	 * 服务端读取到网络数据后到处理
+	 */
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		ByteBuf in = (ByteBuf) msg;/*netty实现的缓冲区*/
+		System.out.println("Server Accept:" + in.toString(CharsetUtil.UTF_8));
+		ctx.write(in);
+	}
+
+	/**
+	 * 服务端读取完成网络数据后的处理
+	 */
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+		ctx.writeAndFlush(Unpooled.EMPTY_BUFFER) //flush掉所有的数据
+			.addListener(ChannelFutureListener.CLOSE);//当flush完成后，关闭连接
+	}
+
+	/**
+	 * 发生异常后的处理
+	 */
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		ctx.close();
+	}
+	
+	
+}
